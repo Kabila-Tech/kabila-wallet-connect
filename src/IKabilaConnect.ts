dbc24b60a57a37154a77671cc8a43b6b18dfa8f9
Rafael Lujan Diaz <rafael.lujan@outlook.com>
2023-10-11 13:56:04 +0200

    First commit

---
+++ src/IKabilaConnect.ts
@@ -0,0 +1,11 @@
+import { KabilaSigner } from "src/entities/KabilaSigner";
+
+export interface IKabilaConnect {
+  accountId: string,
+  signer: KabilaSigner,
+  connect(something: any): Promise<void>,
+  disconnect(disconnect: any): Promise<void>,
+  sign(transaction: string): Promise<string>,
+  execute(transaction: string): Promise<void>,
+  executeAndWait(transaction: string): Promise<void>
+}
---
+++ src/IKabilaConnectImpl.ts
@@ -0,0 +1,32 @@
+import { IKabilaConnect } from "./IKabilaConnect";
+import {KabilaSigner} from "@/entities/KabilaSigner";
+
+export class KabilaConnect implements IKabilaConnect {
+  accountId: string;
+  signer: KabilaSigner;
+
+  constructor(accountId: string) {
+    this.accountId = accountId;
+    this.signer = new KabilaSigner()
+  }
+
+  async connect(onConnect?: Function): Promise<void> {
+    // walletConnector.connect()
+  }
+
+  async disconnect(topic: any, onDisconnect?: Function): Promise<void> {
+    // walletConnector.disconnect()
+  }
+
+  async sign(transaction: string): Promise<string> {
+    return 'null';
+  }
+
+  async execute(transaction: string): Promise<void> {
+    // walletConnector.send
+  }
+
+  async executeAndWait(transaction: string): Promise<void> {
+  }
+
+}
